aea2c9d77f753278c7e7c58b24f6ef256b656d66
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-06-01 16:38:38 +0530

    server side

--- server/server.js
+++ server/server.js
@@ -13,14 +13,13 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 const port = 3500;
-const hostname = 'localhost';
+const hostname = "localhost";
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
   console.log("Created Server");
 
   // res.writeHead(200, {'contet-type' : 'text/plain'});
@@ -32,20 +31,36 @@ const server = http.createServer((req, res)=>{
   const parsed_url = url.parse(req_url);
   console.log("parsed_url : ", parsed_url);
 
-  if(parsed_url.pathname === '/') {
-    res.writeHead(200, {'contet-type' : 'text/html'});
-    res.end(fs.readFilesync('../client/form.html'));
-  }else if(parsed_url.pathname === '/form.css') {
-    res.writeHead(200, {'contet-type' : 'text/css'});
-    res.end(fs.readFilesync('../client/form.css'));
-  }else{
-    res.writeHead(404, {'contet-type' : 'text/plain'});
-    res.end(fs.readFilesync("Page not found"));
+  if (parsed_url.pathname === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(fs.readFileSync("../client/form.html"));
+  }
+   else if (parsed_url.pathname === "/form.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.end(fs.readFileSync("../client/form.css"));
+  } 
+  else if (parsed_url.pathname === "/form.js") {
+    res.writeHead(200, { "content-type": "text/js" });
+    res.end(fs.readFileSync("../client/form.js"));
+  } 
+  else if (parsed_url.pathname === "/form-password.js") {
+    res.writeHead(200, { "content-type": "text/js" });
+    res.end(fs.readFileSync("../client/form-password.js"));
+  }
+  else if (parsed_url.pathname === "/eagle2.jpg") {
+    res.writeHead(200, { "content-type": "image/jpeg" });
+    res.end(fs.readFileSync("../client/eagle2.jpg"));
+  }
+  else if (parsed_url.pathname === "/google.png") {
+    res.writeHead(200, { "content-type": "image/png" });
+    res.end(fs.readFileSync("../client/google.png"));
+  }
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end(fs.readFileSync("../client/404.html"));
   }
 });
 
-server.listen(port, hostname, ()=> {
-  console.log(`server running at http://${hostname}:${port}`)
+server.listen(port, hostname, () => {
+  console.log(`server running at http://${hostname}:${port}`);
 });
-
-
